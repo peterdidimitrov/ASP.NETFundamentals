2ba08ad668be0e971a1df1c0fec7c6509fb2a530
Peter Dimitrov <113916372+peterdidimitrov@users.noreply.github.com>
2024-01-25 18:25:35 +0200

    Add first part of ShoppingListApp

---
+++ ShoppingListApp/ShoppingListApp/Contracts/IProductService.cs
@@ -0,0 +1,19 @@
+﻿using ShoppingListApp.Models;
+
+namespace ShoppingListApp.Contracts
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductViewModel>> GetAllAsync();
+        
+        Task<ProductViewModel> GetByIdAsync(int id);
+
+        //Task<ProductViewModel> GetByNameAsync(string name);
+
+        Task AddProductAsync(ProductViewModel model);
+
+        Task UpdateProductAsync(ProductViewModel model);
+
+        Task DeleteProductAsync(int id);
+    }
+}
