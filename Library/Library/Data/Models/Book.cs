fc6c9299593896ba4cd051833b26feeadf9b85aa
peterdidimitrov <petr_dimitrov@abv.bg>
2024-02-16 22:29:30 +0200

    Create the data base and implement the All method.

---
+++ Library/Library/Data/Models/Book.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static Library.Constants.BookConstants;
+
+namespace Library.Data.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(BookTitleMaxLength)]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(BookAuthorMaxLength)]
+        public string Author { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(BookDescriptionMaxLength)]
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        public string ImageUrl { get; set; } = string.Empty;
+
+        [Required]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Rating { get; set;}
+
+        [Required]
+        public int CategoryId { get; set;}
+
+        [ForeignKey(nameof(CategoryId))]
+        public Category Category { get; set; } = null!;
+
+        public ICollection<IdentityUserBook> UsersBooks { get; set; } = new HashSet<IdentityUserBook>();
+    }
+}
